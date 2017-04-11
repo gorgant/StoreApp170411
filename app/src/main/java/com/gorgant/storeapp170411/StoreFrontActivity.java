1357eea89eed316aa9c313982345048407c9f017
Greg Ishktar <Greg Ishktar>
2017-04-11 18:56:12 -0400

    initial commit

---
+++ app/src/main/java/com/gorgant/storeapp170411/StoreFrontActivity.java
@@ -0,0 +1,13 @@
+package com.gorgant.storeapp170411;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class StoreFrontActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_storefront);
+    }
+}
